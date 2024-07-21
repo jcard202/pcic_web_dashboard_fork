2ffcecc055fcac72918601fc4474e1e91ac87e23
joemar25 <joemar.cardino000@gmail.com>
2024-07-21 15:44:29 +0800

    chore: Refactor Pagination component to improve code readability and maintainability

--- src/routes/utils/dashboard/Pagination.svelte
+++ src/routes/utils/dashboard/Pagination.svelte
@@ -1,7 +1,3 @@
-<!-- 
- mar-note: this is the Pagination component that is used in the .svelte files that are in the src/routes/utils/dashboard folder
-  -->
-
 <script lang="ts">
 	import { Pagination } from 'flowbite-svelte';
 	import { ArrowLeftOutline, ArrowRightOutline } from 'flowbite-svelte-icons';
@@ -13,15 +9,17 @@
 
 	$: startItem = (currentPage - 1) * pageSize + 1;
 	$: endItem = Math.min(currentPage * pageSize, totalItems);
+	$: showPrevious = currentPage > 1;
+	$: showNext = currentPage < totalPages;
 
 	function previous() {
-		if (currentPage > 1) {
+		if (showPrevious) {
 			currentPage--;
 		}
 	}
 
 	function next() {
-		if (currentPage < totalPages) {
+		if (showNext) {
 			currentPage++;
 		}
 	}
@@ -36,26 +34,29 @@
 		<span class="font-semibold text-gray-900 dark:text-white">{totalItems}</span>
 		Entries
 	</div>
-	<Pagination table>
-		<button
-			slot="prev"
-			class="flex items-center gap-2 text-white"
-			on:click={previous}
-			type="button"
-			aria-label="Previous"
-		>
-			<ArrowLeftOutline class="me-2 h-3.5 w-3.5" />
-			Prev
-		</button>
-		<button
-			slot="next"
-			class="flex items-center gap-2 text-white"
-			on:click={next}
-			type="button"
-			aria-label="Next"
-		>
-			Next
-			<ArrowRightOutline class="ms-2 h-6 w-6" />
-		</button>
+	<Pagination
+		{totalPages}
+		{currentPage}
+		on:previous={previous}
+		on:next={next}
+		showControls={true}
+		table={true}
+	>
+		<svelte:fragment slot="prev">
+			{#if showPrevious}
+				<button class="flex items-center gap-2 text-white" type="button" aria-label="Previous">
+					<ArrowLeftOutline class="me-2 h-3.5 w-3.5" />
+					Prev
+				</button>
+			{/if}
+		</svelte:fragment>
+		<svelte:fragment slot="next">
+			{#if showNext}
+				<button class="flex items-center gap-2 text-white" type="button" aria-label="Next">
+					Next
+					<ArrowRightOutline class="ms-2 h-6 w-6" />
+				</button>
+			{/if}
+		</svelte:fragment>
 	</Pagination>
 </div>
