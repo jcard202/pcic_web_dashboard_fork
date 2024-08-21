b482d4f7994630c0405b4a4a10b7a1124c031e72
cleomark <cleocalitisin@gmail.com>
2024-08-21 10:26:48 +0800

    uniform pagination - task management

--- src/routes/utils/dashboard/Pagination.svelte
+++ src/routes/utils/dashboard/Pagination.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
-	import { Pagination } from 'flowbite-svelte';
-	import { ArrowLeftOutline, ArrowRightOutline } from 'flowbite-svelte-icons';
-
 	export let currentPage: number;
 	export let totalPages: number;
-	export let pageSize: number;
-	export let totalItems: number;
 
-	$: startItem = (currentPage - 1) * pageSize + 1;
-	$: endItem = Math.min(currentPage * pageSize, totalItems);
 	$: showPrevious = currentPage > 1;
 	$: showNext = currentPage < totalPages;
 
@@ -25,40 +18,28 @@
 	}
 </script>
 
-<div class="flex flex-col items-center justify-center gap-2 mb-2">
-	<div class="text-sm text-gray-700 dark:text-gray-400">
-		Showing <span class="font-semibold text-gray-900 dark:text-white">{startItem}</span>
-		to
-		<span class="font-semibold text-gray-900 dark:text-white">{endItem}</span>
-		of
-		<span class="font-semibold text-gray-900 dark:text-white">{totalItems}</span>
-		Entries
-	</div>
-	{#if showPrevious || showNext}
-		<Pagination
-			{totalPages}
-			{currentPage}
-			on:previous={previous}
-			on:next={next}
-			showControls={true}
-			table={true}
-		>
-			<svelte:fragment slot="prev">
-				{#if showPrevious}
-					<button class="flex items-center gap-2 text-white" type="button" aria-label="Previous">
-						<ArrowLeftOutline class="me-2 h-3.5 w-3.5" />
-						Prev
-					</button>
-				{/if}
-			</svelte:fragment>
-			<svelte:fragment slot="next">
-				{#if showNext}
-					<button class="flex items-center gap-2 text-white" type="button" aria-label="Next">
-						Next
-						<ArrowRightOutline class="ms-2 h-6 w-6" />
-					</button>
-				{/if}
-			</svelte:fragment>
-		</Pagination>
-	{/if}
+<div class="mt-4 flex items-center justify-between rounded-b-lg bg-gray-800 py-3">
+	<button
+		class="text-md rounded-md px-5 py-2 font-medium {showPrevious
+			? 'bg-gray-700 text-white'
+			: 'bg-gray-700 text-gray-500'}"
+		on:click={previous}
+		disabled={!showPrevious}
+	>
+		Previous
+	</button>
+
+	<span class="text-gray-400">
+		Page {currentPage} of {totalPages}
+	</span>
+
+	<button
+		class="text-md rounded-md px-5 py-2 font-medium {showNext
+			? 'bg-blue-600 text-white'
+			: 'bg-gray-700 text-gray-500'}"
+		on:click={next}
+		disabled={!showNext}
+	>
+		Next
+	</button>
 </div>
