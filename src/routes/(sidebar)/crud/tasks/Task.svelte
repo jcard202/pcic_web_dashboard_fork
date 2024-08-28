995cd6c2758f3efbdc468e4020f2f18ab2196911
cleomark <cleocalitisin@gmail.com>
2024-08-28 09:03:45 +0800

    tasks > add task > assignee must be dropdown

--- src/routes/(sidebar)/crud/tasks/Task.svelte
+++ src/routes/(sidebar)/crud/tasks/Task.svelte
@@ -1,30 +1,44 @@
 <script lang="ts">
-	import { Button, CloseButton, Heading, Input, Label, Modal, Select, Textarea } from 'flowbite-svelte';
-	import { CheckCircleOutline, CloseOutline, ExclamationCircleOutline, QuestionCircleOutline } from 'flowbite-svelte-icons';
+	import {
+		Button,
+		CloseButton,
+		Heading,
+		Input,
+		Label,
+		Modal,
+		Select,
+		Textarea
+	} from 'flowbite-svelte';
+	import {
+		CheckCircleOutline,
+		CloseOutline,
+		ExclamationCircleOutline,
+		QuestionCircleOutline
+	} from 'flowbite-svelte-icons';
 	import { onMount } from 'svelte';
 	export let hidden: boolean = true; // modal control
-	export let users:any[] = [];
-	export let selected_task:any = {};
-	export let upsertTask:any = null;
-	export let markAsComplete:any = null;
-	export let clearForm:any = null;
-	export let formView:any;
-	let filteredUsers:any[] = [...users];
+	export let users: any[] = [];
+	export let selected_task: any = {};
+	export let upsertTask: any = null;
+	export let markAsComplete: any = null;
+	export let clearForm: any = null;
+	export let formView: any;
+	let filteredUsers: any[] = [...users];
 
-	let task_name:string;
-	let service_group:string;
-	let priority:string;
-	let status:string = 'for dispatch';
-	let ppir_form:any;
+	let task_name: string;
+	let service_group: string;
+	let priority: string;
+	let status: string = 'for dispatch';
+	let ppir_form: any;
 
-	let open:boolean = false;
+	let open: boolean = false;
 
-	let completeWarning:boolean = true;
+	let completeWarning: boolean = true;
 	let viewForm = false;
 
-	onMount(()=>{
-		if(selected_task){
-			selected_user =  users.find(user => user.id == selected_task.users.id)
+	onMount(() => {
+		if (selected_task) {
+			selected_user = users.find((user) => user.id == selected_task.users.id) || null;
 			task_name = selected_task.task_number;
 			service_group = selected_task.service_group;
 			status = selected_task.status;
@@ -35,79 +49,79 @@
 		console.log(formView);
 	});
 
+	let selected_user: any = null;
 
-	let selected_user:any = {};
-
-	const filterUsers = (event:any) => {
+	const filterUsers = (event: any) => {
 		const query = event.target.value.toLowerCase();
-		filteredUsers = users.filter(user => user.inspector_name.toLowerCase().includes(query));
+		filteredUsers = users.filter((user) => user.inspector_name.toLowerCase().includes(query));
 	};
-	const handleUserChange = (user:any) => {
-		 selected_user = user;
+	const handleUserChange = (event: Event) => {
+		const select = event.target as HTMLSelectElement;
+		selected_user = users.find((user) => user.id === select.value);
 	};
-	const handlePOChange = (event:any) => {
-		 service_group = event.target.value
+	const handlePOChange = (event: any) => {
+		service_group = event.target.value;
 	};
-	const handlePrioChange = (event:any) => {
-		 priority = event.target.value
+	const handlePrioChange = (event: any) => {
+		priority = event.target.value;
 	};
 
-
-	const handleTaskNameChange = (event:any) => {
+	const handleTaskNameChange = (event: any) => {
 		task_name = event.target.value;
-	}
+	};
 
-	const getPOFromIndex = (index:number) => {
-		switch(index){
+	const getPOFromIndex = (index: number) => {
+		switch (index) {
 			case 4:
 				return 'PO4A';
 			case 5:
 				return 'PO4B';
 			default:
-				if(index < 4){
-					return 'PO' + index
-				}else{
-					return 'PO' + (index-1)
+				if (index < 4) {
+					return 'PO' + index;
+				} else {
+					return 'PO' + (index - 1);
 				}
 		}
-	}
+	};
 
 	const getRegionFromPO = (index: number) => {
-		switch(index){
+		switch (index) {
 			case 4:
 				return '04A';
 			case 5:
 				return '04B';
 			default:
-				if(index < 4){
+				if (index < 4) {
 					return index;
-				}else{
-					return index-1;
+				} else {
+					return index - 1;
 				}
 		}
-	}
+	};
 
-	const getTypeFromPO = (PO:string) =>{
-		switch(PO){
+	const getTypeFromPO = (PO: string) => {
+		switch (PO) {
 			case 'PO4A':
 				return 'Region 04A PPIR';
 			case 'PO4B':
 				return 'Region 04B PPIR';
 			default:
-				if(!PO){
+				if (!PO) {
 					return null;
 				}
-				if(PO.split('O')[1].length > 1){
-					return 'Region ' + PO.split('O')[1] +' PPIR';
-				}else{
-					return 'Region 0' + PO.split('O')[1] +' PPIR';
+				if (PO.split('O')[1].length > 1) {
+					return 'Region ' + PO.split('O')[1] + ' PPIR';
+				} else {
+					return 'Region 0' + PO.split('O')[1] + ' PPIR';
 				}
 		}
-	}
-
+	};
 </script>
 
-<Heading tag="h5" class="mb-6 text-sm font-semibold uppercase">{selected_task ? 'Update Task' : 'Add New Task'}</Heading>
+<Heading tag="h5" class="mb-6 text-sm font-semibold uppercase"
+	>{selected_task ? 'Update Task' : 'Add New Task'}</Heading
+>
 <CloseButton
 	on:click={() => (hidden = true)}
 	class="absolute right-2.5 top-2.5 text-gray-400 hover:text-black dark:text-white"
@@ -127,29 +141,34 @@
 			/>
 		</Label>
 
-		
 		<Label class="space-y-2">
 			<span>Service Group</span>
 			<Select class="border-gray-300 font-normal outline-none" on:change={handlePOChange} required>
 				<option value={null} selected>Select Type</option>
 				{#each Array.from({ length: 17 }, (_, i) => i) as num}
-					{#if service_group == getPOFromIndex(num+1)}
-						<option selected value="{getPOFromIndex(num+1)}">Region {getRegionFromPO(num+1)}</option>
+					{#if service_group == getPOFromIndex(num + 1)}
+						<option selected value={getPOFromIndex(num + 1)}
+							>Region {getRegionFromPO(num + 1)}</option
+						>
 					{:else}
-						<option value="{getPOFromIndex(num+1)}">Region {getRegionFromPO(num+1)}</option>
+						<option value={getPOFromIndex(num + 1)}>Region {getRegionFromPO(num + 1)}</option>
 					{/if}
 				{/each}
 			</Select>
 		</Label>
-		
 
 		<Label class="space-y-2">
 			<span>Service Type</span>
-			<Input name="group" class="border font-normal outline-none" readonly placeholder="None"
+			<Input
+				name="group"
+				class="border font-normal outline-none"
+				readonly
+				placeholder="None"
 				value={service_group ? getTypeFromPO(service_group) : ''}
-			required />
+				required
+			/>
 		</Label>
-<!-- 
+		<!-- 
 		<Label class="space-y-2">
 			<span>Description</span>
 			<Textarea
@@ -161,13 +180,17 @@
 
 		<Label class="space-y-2">
 			<span>Priority</span>
-			<Select class="border-gray-300 font-normal outline-none" on:change={handlePrioChange} required>
+			<Select
+				class="border-gray-300 font-normal outline-none"
+				on:change={handlePrioChange}
+				required
+			>
 				<option value={null} selected>Set Priority</option>
 				{#each ['Low', 'Medium', 'High'] as prio}
 					{#if priority == prio}
-						<option selected value="{prio}">{prio}</option>
+						<option selected value={prio}>{prio}</option>
 					{:else}
-						<option value="{prio}">{prio}</option>
+						<option value={prio}>{prio}</option>
 					{/if}
 				{/each}
 			</Select>
@@ -175,83 +198,86 @@
 
 		<Label class="space-y-2">
 			<span>Assignee</span>
-			<Input
+			<Select
 				name="assignee"
 				class="border font-normal outline-none"
-				placeholder="Assigned user"
-				readonly
-				value={(selected_user) ? selected_user.inspector_name : ''}
-				on:input={filterUsers}
+				bind:value={selected_user}
 				required
-			/>
-			<div class="w-full px-2"> 
-				<div class="w-full h-[2px] opacity-40 dark:bg-white bg-black px-5"></div>
-			</div>
-			<Input
-				name="title"
-				class="border font-normal outline-none"
-				placeholder="Search users..."
-				on:input={filterUsers}
-			/>
-			<div class= 'w-full h-48 bg-black/[0.05] rounded-lg overflow-y-scroll p-2'>
-				{#each filteredUsers as user}
-					<Button on:click={()=>handleUserChange(user)} class="w-full mb-2">{user.inspector_name}</Button>
+			>
+				<option value={null}>Select an assignee</option>
+				{#each users as user}
+					<option value={user}>{user.inspector_name}</option>
 				{/each}
-			</div>
+			</Select>
 		</Label>
 
 		{#if selected_task}
 			<Label class="space-y-2">
 				<span>PPIR Form</span>
-				<Button color='green' on:click={()=>{
-					open=true;
-					viewForm = true;
-				}}  class="w-full mb-2">View Form</Button>
-				<Button on:click={()=>{
-					open=true;
-					viewForm = false;
-					completeWarning = false;
-				}} color='red' class="w-full mb-2">Clear Form</Button>
+				<Button
+					color="green"
+					on:click={() => {
+						open = true;
+						viewForm = true;
+					}}
+					class="mb-2 w-full">View Form</Button
+				>
+				<Button
+					on:click={() => {
+						open = true;
+						viewForm = false;
+						completeWarning = false;
+					}}
+					color="red"
+					class="mb-2 w-full">Clear Form</Button
+				>
 			</Label>
 
 			<!-- {#if selected_task.status != 'completed'} -->
-				<Label class="space-y-2">
-					<span>Status</span>
-					<Button on:click={()=>{
-						if(selected_task.status == 'completed'){
+			<Label class="space-y-2">
+				<span>Status</span>
+				<Button
+					on:click={() => {
+						if (selected_task.status == 'completed') {
 							return;
 						}
 						open = true;
 						viewForm = false;
 						completeWarning = true;
-					}} color={selected_task.status =='completed' ? 'green': 'blue'} class="w-full mb-2">{selected_task.status =='completed' ? 'Completed': 'Mark as Complete'}</Button>
-				</Label>
+					}}
+					color={selected_task.status == 'completed' ? 'green' : 'blue'}
+					class="mb-2 w-full"
+					>{selected_task.status == 'completed' ? 'Completed' : 'Mark as Complete'}</Button
+				>
+			</Label>
 			<!-- {/if} -->
 		{/if}
 
 		<div class="flex w-full justify-center space-x-4 pb-4">
-			<Button on:click = {async()=>{
-				if(task_name.trim() == '' && task_name.length > 0){
-					task_name = '';
-					return;
-				}
-				const success = await upsertTask({
-					id : selected_task?.id,
-					task_number: task_name,
-					service_group: service_group,
-					service_type: getTypeFromPO(service_group),
-					priority: priority,
-					assignee: selected_user.id,
-					status: status,
-					task_type: 'ppir',
-				})
+			<Button
+				on:click={async () => {
+					if (task_name.trim() == '' && task_name.length > 0) {
+						task_name = '';
+						return;
+					}
+					const success = await upsertTask({
+						id: selected_task?.id,
+						task_number: task_name,
+						service_group: service_group,
+						service_type: getTypeFromPO(service_group),
+						priority: priority,
+						assignee: selected_user ? selected_user.id : null,
+						status: status,
+						task_type: 'ppir'
+					});
 
-				if(success){
-					hidden = true;
-				}
-				
-				
-			}} type="submit" class="w-full">{selected_task ? 'Update Task' : 'Add Task'}</Button>
+					if (success) {
+						hidden = true;
+					}
+				}}
+				type="submit"
+				class="w-full">{selected_task ? 'Update Task' : 'Add Task'}</Button
+			>
 			<Button color="alternative" class="w-full" on:click={() => (hidden = true)}>
 				<CloseOutline />
 				Cancel
@@ -269,29 +295,32 @@
 		{/if}
 
 		<h3 class="mb-6 text-center text-lg text-gray-500 dark:text-gray-400">
-			{ completeWarning ? 
-				"Are you sure you want to mark this task as completed?":
-				"Are you sure you want reset the assignee's form?"
-			}
+			{completeWarning
+				? 'Are you sure you want to mark this task as completed?'
+				: "Are you sure you want reset the assignee's form?"}
 		</h3>
 
 		<div class="flex items-center justify-center">
-			<Button color={completeWarning ? "blue" : "red"} class="mr-2" on:click={ async()=>{
-				if(completeWarning){
-					const success = await markAsComplete(selected_task.id)
-					if(success){
-						selected_task.status = 'completed';
+			<Button
+				color={completeWarning ? 'blue' : 'red'}
+				class="mr-2"
+				on:click={async () => {
+					if (completeWarning) {
+						const success = await markAsComplete(selected_task.id);
+						if (success) {
+							selected_task.status = 'completed';
+						}
+					} else {
+						clearForm(selected_task.id);
 					}
-				}else{
-					clearForm(selected_task.id);
-				}
-				open = false;
-				hidden = true;
-			}}>Yes, I'm sure</Button>
+					open = false;
+					hidden = true;
+				}}>Yes, I'm sure</Button
+			>
 			<Button color="alternative" on:click={() => (open = false)}>No, cancel</Button>
 		</div>
 	{:else}
-		<object  data={formView} width="100%" height="600px"  title="form"></object>
+		<object data={formView} width="100%" height="600px" title="form"></object>
 		<div class="flex items-center justify-center">
 			<Button color="alternative" on:click={() => (open = false)}>Close</Button>
 		</div>
